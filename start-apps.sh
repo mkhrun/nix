54d8c143b320d65ea28f6b8373e30a9d24453ede
Maksym Khrun <maksym.khrun@gmail.com>
2018-09-10 18:31:23 +0300

    Updated jar's naming

--- start-apps.sh
+++ start-apps.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -jar eurekaserver-0.0.1-SNAPSHOT.jar & java -jar one-0.0.1-SNAPSHOT.jar & java -jar two-0.0.1-SNAPSHOT.jar
+java -jar eurekaserver.jar & java -jar one.jar & java -jar two.jar
